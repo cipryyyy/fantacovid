41564e0500657b8129103b13e646a772395a0e10
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2021-04-05 14:44:07 +0200

    Update Telegram_bot.js

--- Telegram_bot.js
+++ Telegram_bot.js
@@ -1,59 +1,51 @@
-import requests
-from bs4 import BeautifulSoup as BS
-import pandas as pd
+var time = require('./fanta_lib.js');
+require("dotenv").config({path: "./Tokens.env"});
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.FANTA_TOKEN);
+const fs = require('fs');
 
-def table_gen(src="https://github.com/pcm-dpc/COVID-19/blob/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json"):
-    table=[]
-    switch=0
-    response=requests.get(src)
-    soup = BS(response.content, "lxml")
-    rows = soup.find_all("tr")
-    for row in rows:
-        cell = row.find_all("td")
-        span = cell[1].find_all("span", class_=["pl-s", "pl-c1"])
-        pack = []
-        for text in span:
-            switch += 1
-            try:
-                data = int(text.text.replace('"', ''))
-            except ValueError:
-                data = text.text.replace('"', '')
-                
-            pack.append(data)
-            if switch == 2:
-                switch = 0
-                table.append(pack)
-                break
-    return table
+var recorder = function (player_name, player_id, group_name, group_id, bet) {
+    var data = player_name+","+player_id+","+group_name+","+group_id+","+bet+"\n";
+    fs.appendFile('./player.csv', data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+};
 
-def today():
-    table=table_gen()
-    for row in table:
-        if row[0]=="nuovi_positivi":
-            return row[1]
+bot.help((ctx) =>
+    ctx.reply("Manda /gioca e rispondi al messaggio per giocare\n\n"+
+    "Il vincitore del gruppo verrà estratto alle 20, i numeri potranno essere giocati entro le 17:00")
+);
 
-def closest(bets, target=today()):
-    minimum = float('inf')
-    for bet in bets:
-        if abs(target-bet)<minimum:
-            minimum = bet
-    return bet
+bot.command("gioca", (ctx) => ctx.reply("Rispondi a questo messaggio con la tua scommessa"));
 
-def cleaner(path="/home/andrea/Documents/Programmazione/FANTA/player.csv"):
-    df = pd.read_csv(path)
-    df = df.drop_duplicates(subset=["plr_id"], keep="last")
-    print(df)
-    df.to_csv(path, index=False)
+bot.on("text", (ctx) => {
+    console.log(time.hour()+":"+time.minute());
+    if (time.hour()<17) {
+        if (!isNaN(ctx.update.message.text.toString())){
+            player_id = ctx.update.message.from.id.toString();
+            player_display = ctx.update.message.from.first_name.toString();
+            group_id = ctx.update.message.chat.id.toString();
+            group_name = ctx.update.message.chat.title.toString();
 
-if __name__=="__main__":
-    print(*table_gen(), sep="\n")
-    today=today()
-    print(f"I casi di oggi sono {today}")
-    
-    cleaner()       #save only the last guess
-    df = pd.read_csv("/home/andrea/Documents/Programmazione/FANTA/player.csv")
-    bets = list(df["bet"])
-    players = list(df["plr"])
-    winner = closest(bets)
-    print(winner)
-    print(f"Il vincitore è {players[bets.index(winner)]} con {winner} casi!\n\nTotale casi di oggi: {today}\nScarto: {abs(today-winner)}")
+            bet = parseInt(ctx.update.message.text.toString());
+            recorder(player_display, player_id, group_name, group_id, bet);
+            ctx.reply("_Salvato!_\n"+player_display+": "+bet+" casi", {parse_mode: "markdown"});
+        } else {
+            try {
+                user = "@"+ctx.update.message.from.username.toString();
+            } catch {
+                user = ctx.update.message.from.first_name.toString();
+            }
+            ctx.reply(user+" rispondi con un numero.");
+        }
+    } else {
+        ctx.reply("I numeri vanno giocati entro le 17:00!");
+    }
+})
+
+bot.launch()
+console.log("Bot online")
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
